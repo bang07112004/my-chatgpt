a8dc7c466bddb6b3bfa9f236067a958fe28fe7bf
bang07112004 <phong0909191145@gmail.com>
2023-02-10 12:16:39 +0700

    update

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -18,17 +18,14 @@ function SideBar() {
   );
 
   return (
-    <div className="p-2 flex sm:flex-row md:flex-col h-screen">
-      <div className="flex-1 flex sm:flex-row md:flex-col">
+    <div className="p-2 flex flex-row md:flex-col h-screen">
+      <div className="flex-1 flex flex-row md:flex-col">
         <div>
-          {/* NewChat */}
           <NewChat />
           <div className="hidden sm:inline">
-            {/* ModelSelections */}
-
             <ModelSelection />
           </div>
-          <div className="flex sm:flex-row md:flex-col space-y-2 my-2">
+          <div className="flex flex-row md:flex-col space-y-2 my-2">
             {loading && (
               <div className="animate-pulse text-center text-white flex items-center gap-2">
                 <img src={load.src} alt="" className="w-20 h-20" />
