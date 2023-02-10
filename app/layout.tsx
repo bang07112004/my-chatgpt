1f1d58b2ea24d50813e01bb134ae804d6a3675ba
bang07112004 <phong0909191145@gmail.com>
2023-02-10 11:58:19 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({
           {!session ? (
             <Login />
           ) : (
-            <div className="grid grid-cols-[1fr_3fr]">
+            <div className="flex">
               <div className="bg-[#202123] h-screen overflow-y-auto md:min-w-[20rem]">
                 <SideBar />
               </div>
